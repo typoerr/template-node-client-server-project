241ffda5051a90f3a2bdc49ff4dbfc581d824a42
typoerr <masahirokato.dev@gmail.com>
2020-04-04 11:20:51 +0900

    update(task): clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-exports['build:client'] = require('./task/build-clinet').build
-exports['watch:client'] = require('./task/build-clinet').watch
-exports['build:server'] = require('./task/build-server').build
-exports['watch:server'] = require('./task/build-server').watch
-exports.clean = require('./task/clean-dist')
+const gulp = require('gulp')
+
+const client = require('./task/build-clinet')
+const server = require('./task/build-server')
+const clean = require('./task/clean-dist')
+
+exports['build:client'] = client.build
+exports['watch:client'] = client.watch
+exports['build:server'] = server.build
+exports['watch:server'] = server.watch
+exports['clean'] = clean
+exports['build'] = gulp.parallel(client.build, server.build)
+exports['watch'] = gulp.parallel(client.watch, server.watch)
